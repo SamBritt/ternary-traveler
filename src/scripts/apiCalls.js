b4a8cc7159bf3c9ec0ebedc3f7ff7f8be265752f
SamBritt <sam.britt2@yahoo.com>
2019-04-10 14:37:17 -0500

    items displaying to DOM

--- src/scripts/apiCalls.js
+++ src/scripts/apiCalls.js
@@ -1,8 +1,10 @@
+import struct from "./domStructure"
+
 const apiURL = "http://localhost:8088";
 
 const API = {
     getPointsOfInterest(){
-        return fetch(`${apiURL}/interests`)
+        return fetch(`${apiURL}/places?_embed=interests`)
             .then(r => r.json())
     }
 }
